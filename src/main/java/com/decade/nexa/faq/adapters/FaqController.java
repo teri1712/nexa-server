503c35920a0669fdad55b5fa39084456cbc00589
decade <20120220@student.hcmus.edu.vn>
2026-06-04 02:09:52 +0700

    feat: Add FAQ endpoint, update healthcheck, and improve API docs
    - Added `/faqs` endpoint with `FaqController` and Swagger documentation.
    - Enhanced healthcheck with `start_interval` configuration in Compose.
    - Refined API documentation with clearer responses, new schemas (`FaqResponse`, `DocumentResponse`), and validation updates.
    - Adjusted configuration files to remove sensitive data and streamline environment-specific settings.

--- src/main/java/com/decade/nexa/faq/adapters/FaqController.java
+++ src/main/java/com/decade/nexa/faq/adapters/FaqController.java
@@ -2,6 +2,7 @@ package com.decade.nexa.faq.adapters;
 
 import com.decade.nexa.faq.FaqResponse;
 import com.decade.nexa.faq.application.query.FaqService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-
 @RequiredArgsConstructor
 public class FaqController {
     final FaqService faqService;
 
+    @Operation(summary = "Get today FAQ list")
     @GetMapping("/faqs")
     public List<FaqResponse> faqs() {
         return faqService.list();
